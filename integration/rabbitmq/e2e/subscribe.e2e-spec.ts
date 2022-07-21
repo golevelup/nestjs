7a9eb7d710d9d1d4b87e777fee4b375546b347ea
Craig Otis <craig.otis@hingehealth.com>
2022-07-21 11:38:57 -0400

    Adds a delay between MQ publish and callback assertions to allow for messages to arrive

--- integration/rabbitmq/e2e/subscribe.e2e-spec.ts
+++ integration/rabbitmq/e2e/subscribe.e2e-spec.ts
@@ -185,18 +185,21 @@ describe('Rabbit Subscribe', () => {
       );
       // Make sure the subscription is functional
       amqpConnection.publish(exchange, routingKey1, `testMessage-1`);
+      await new Promise((resolve) => setTimeout(resolve, 50));
       expect(subscriptionCallback).toHaveBeenCalledWith(`testMessage-1`);
       subscriptionCallback.mockReset();
       // Perform cancel using consumer tag
       amqpConnection.channel.cancelConsumer(consumerTag);
       // Make sure the subscription has been canceled
       amqpConnection.publish(exchange, routingKey1, `testMessage-2`);
+      await new Promise((resolve) => setTimeout(resolve, 50));
       expect(subscriptionCallback).not.toHaveBeenCalled();
       subscriptionCallback.mockReset();
       // Resume the consumer
       const newConsumerTag = amqpConnection.channel.resumeConsumer(consumerTag);
       // Make sure the subscription was reestablished
       amqpConnection.publish(exchange, routingKey1, `testMessage-3`);
+      await new Promise((resolve) => setTimeout(resolve, 50));
       expect(subscriptionCallback).toHaveBeenCalledWith(`testMessage-3`);
     });
   });
