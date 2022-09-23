b34aac556aa23abd79a3698aaae51933ee39038f
Franco Mangone <franco.mangone@agrotoken.io>
2022-09-23 17:42:40 -0400

    Adds `createExchangeIfNotExists` option to interface

--- packages/rabbitmq/src/amqp/connection.ts
+++ packages/rabbitmq/src/amqp/connection.ts
@@ -265,19 +265,14 @@ export class AmqpConnection {
       // Always assert exchanges & rpc queue in default channel.
       this.config.exchanges.forEach((x) => {
         if (x.createExchangeIfNotExists) {
-            return channel.assertExchange(
-              x.name,
-              x.type || this.config.defaultExchangeType,
-              x.options
-            )
+          return channel.assertExchange(
+            x.name,
+            x.type || this.config.defaultExchangeType,
+            x.options
+          );
         }
-        return channel.checkExchange(
-          x.name,
-          x.type || this.config.defaultExchangeType,
-          x.options
-        )
+        return channel.checkExchange(x.name);
       });
-
 
       if (this.config.enableDirectReplyTo) {
         await this.initDirectReplyQueue(channel);
