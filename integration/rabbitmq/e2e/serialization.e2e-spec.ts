8ee7c7732b395b7ae4155d342433ab0423ce1fb8
dafow <falah.salim@gmail.com>
2024-08-05 02:31:18 -0400

    feat(connection.ts): adds deserializer options to message handlers (closes #704)
    Allows passing a 'deserializer' function to message handler options that override the module's
    deserialization function

--- integration/rabbitmq/e2e/serialization.e2e-spec.ts
+++ integration/rabbitmq/e2e/serialization.e2e-spec.ts
@@ -2,27 +2,56 @@ import {
   AmqpConnection,
   RabbitMQModule,
   RabbitSubscribe,
+  MessageSerializer,
+  MessageDeserializer,
 } from '@golevelup/nestjs-rabbitmq';
 import { INestApplication, Injectable, LoggerService } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
 import { createMock } from '@golevelup/ts-jest';
 import { gzipSync, gunzipSync } from 'node:zlib';
 
-const testHandler = jest.fn();
+const moduleSerializeHandler = jest.fn();
+const handlerSerializeHandler = jest.fn();
+
+const moduleSerializer: MessageSerializer = jest.fn((value) =>
+  gzipSync(JSON.stringify(value)),
+);
+const moduleDeserializer: MessageDeserializer = jest.fn((message) =>
+  JSON.parse(gunzipSync(message).toString()),
+);
+
+const handlerDeserializer: MessageDeserializer = jest.fn((message) =>
+  JSON.parse(gunzipSync(message).toString()),
+);
 
 const exchange = 'testSerializationExchange';
 const routingKey1 = 'testSerializationRoute1';
 const routingKey2 = 'testSerializationRoute2';
+const routingKey3 = 'testSerializationRoute3';
+const routingKey4 = 'testSerializationRoute4';
 
 @Injectable()
-class SubscribeService {
+class SubscribeModuleSerializationService {
   @RabbitSubscribe({
     exchange,
     routingKey: [routingKey1, routingKey2],
-    queue: 'serializeQueue',
+    queue: 'moduleSerializeQueue',
   })
   handleSubscribe(message: object) {
-    testHandler(message);
+    moduleSerializeHandler(message);
+  }
+}
+
+@Injectable()
+class SubscribeHandlerSerializationService {
+  @RabbitSubscribe({
+    exchange,
+    routingKey: [routingKey3, routingKey4],
+    queue: 'handlerSerializeQueue',
+    deserializer: handlerDeserializer,
+  })
+  handleSubscribe(message: object) {
+    handlerSerializeHandler(message);
   }
 }
 
@@ -39,9 +68,14 @@ describe('Rabbit Subscribe', () => {
     process.env.NODE_ENV === 'ci' ? process.env.RABBITMQ_PORT : '5672';
   const uri = `amqp://rabbitmq:rabbitmq@${rabbitHost}:${rabbitPort}`;
 
-  beforeAll(async () => {
+  beforeEach(async () => {
+    jest.clearAllMocks();
+
     const moduleFixture = await Test.createTestingModule({
-      providers: [SubscribeService],
+      providers: [
+        SubscribeModuleSerializationService,
+        SubscribeHandlerSerializationService,
+      ],
       imports: [
         RabbitMQModule.forRoot(RabbitMQModule, {
           exchanges: [
@@ -53,12 +87,8 @@ describe('Rabbit Subscribe', () => {
           uri,
           connectionInitOptions: { wait: true, reject: true, timeout: 3000 },
           logger: customLogger,
-          serializer: (value) => {
-            return gzipSync(JSON.stringify(value));
-          },
-          deserializer: (message) => {
-            return JSON.parse(gunzipSync(message).toString());
-          },
+          serializer: moduleSerializer,
+          deserializer: moduleDeserializer,
         }),
       ],
     }).compile();
@@ -72,19 +102,33 @@ describe('Rabbit Subscribe', () => {
     await app?.close();
   });
 
-  beforeEach(() => {
-    jest.resetAllMocks();
-  });
-
-  it('should receive subscribe messages and handle them', async () => {
+  it('should receive subscribe messages and serialize/deserialize them via module options', async () => {
     [routingKey1, routingKey2].forEach((x, i) =>
       amqpConnection.publish(exchange, x, `testMessage-${i}`),
     );
 
     await new Promise((resolve) => setTimeout(resolve, 50));
 
-    expect(testHandler).toHaveBeenCalledTimes(2);
-    expect(testHandler).toHaveBeenCalledWith(`testMessage-0`);
-    expect(testHandler).toHaveBeenCalledWith(`testMessage-1`);
+    expect(moduleSerializeHandler).toHaveBeenCalledTimes(2);
+    expect(moduleSerializeHandler).toHaveBeenCalledWith(`testMessage-0`);
+    expect(moduleSerializeHandler).toHaveBeenCalledWith(`testMessage-1`);
+    expect(moduleDeserializer).toHaveBeenCalledTimes(2);
+    expect(moduleSerializer).toHaveBeenCalledTimes(2);
+    expect(handlerDeserializer).not.toBeCalled();
+  });
+
+  it('should receive subscribe messages and deserialize via handler options', async () => {
+    [routingKey3, routingKey4].forEach((x, i) =>
+      amqpConnection.publish(exchange, x, `testMessage-${i}`),
+    );
+
+    await new Promise((resolve) => setTimeout(resolve, 50));
+
+    expect(handlerSerializeHandler).toHaveBeenCalledTimes(2);
+    expect(handlerSerializeHandler).toHaveBeenCalledWith(`testMessage-0`);
+    expect(handlerSerializeHandler).toHaveBeenCalledWith(`testMessage-1`);
+    expect(handlerDeserializer).toHaveBeenCalledTimes(2);
+    expect(moduleDeserializer).not.toBeCalled();
+    expect(moduleSerializer).toHaveBeenCalledTimes(2);
   });
 });
