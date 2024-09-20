8898269d6f0dfcabccefc3f0fba0f56d062b4f72
Calvin <calvin@orderful.com>
2024-09-20 15:54:28 -0700

    fix(rabbitmq): fix e2e test uri

--- integration/rabbitmq/e2e/subscribe.e2e-spec.ts
+++ integration/rabbitmq/e2e/subscribe.e2e-spec.ts
@@ -191,7 +191,7 @@ describe('Rabbit Subscribe', () => {
     process.env.NODE_ENV === 'ci' ? process.env.RABBITMQ_HOST : 'localhost';
   const rabbitPort =
     process.env.NODE_ENV === 'ci' ? process.env.RABBITMQ_PORT : '5672';
-  const uri = `amqp://guest:guest@${rabbitHost}:${rabbitPort}`;
+  const uri = `amqp://rabbitmq:rabbitmq@${rabbitHost}:${rabbitPort}`;
 
   beforeAll(async () => {
     const moduleFixture = await Test.createTestingModule({
