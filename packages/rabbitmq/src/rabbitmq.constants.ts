7049c07d678c7d272744dbf0ace1056b7d74d3b9
Rodrigo <monstawoodwow@gmail.com>
2024-09-30 13:33:29 +0200

    feat: small performance improvement using Map over array

--- packages/rabbitmq/src/rabbitmq.constants.ts
+++ packages/rabbitmq/src/rabbitmq.constants.ts
@@ -3,3 +3,5 @@ export const RABBIT_CONFIG_TOKEN = Symbol('RABBIT_CONFIG');
 export const RABBIT_PARAM_TYPE = 3;
 export const RABBIT_HEADER_TYPE = 4;
 export const RABBIT_REQUEST_TYPE = 5;
+export const RABBIT_CONTEXT_TYPE_KEY = 'rmq';
+export const PRECONDITION_FAILED_CODE = 406;
