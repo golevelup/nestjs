933e36dc005772d6413c2f2ce63947c93f803639
Rodrigo <v-rrodrigues@circadianrisk.com>
2025-05-30 20:57:06 +0100

    chore: upgrade deps including stripe

--- packages/stripe/src/stripe.module.ts
+++ packages/stripe/src/stripe.module.ts
@@ -47,7 +47,7 @@ import { StripeWebhookService } from './stripe.webhook.service';
       useFactory: ({
         apiKey,
         typescript = true,
-        apiVersion = '2025-03-31.basil',
+        apiVersion = '2025-05-28.basil',
         ...options
       }: StripeModuleConfig): Stripe => {
         return new Stripe(apiKey, {
