e44298ad81ebf9de305820861c577b688b0c077d
Rodrigo <monstawoodwow@gmail.com>
2026-02-02 17:00:56 +0000

    chore: devDependencies upgrade such as Vite and Stripe upgrade (#1129)
    * chore: update several deps
    
    * chore: include changeset

--- packages/stripe/src/stripe.module.ts
+++ packages/stripe/src/stripe.module.ts
@@ -52,7 +52,7 @@ import { StripeWebhookService } from './stripe.webhook.service';
       useFactory: ({
         apiKey,
         typescript = true,
-        apiVersion = '2025-12-15.clover',
+        apiVersion = '2026-01-28.clover',
         ...options
       }: StripeModuleConfig): Stripe => {
         return new Stripe(apiKey, {
